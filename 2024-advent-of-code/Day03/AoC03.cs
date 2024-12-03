92842f9601dbf6d8a803e299dc3d6ecc15108a70
David Gómez <ldavid.gomez@gmail.com>
2024-12-03 16:17:39 +0100

    Aoc Day03 part2

--- 2024-advent-of-code/Day03/AoC03.cs
+++ 2024-advent-of-code/Day03/AoC03.cs
@@ -6,27 +6,68 @@ namespace _2024_advent_of_code.Day03;
 public partial class AoC03 : AoC
 {
     [GeneratedRegex(@"mul\((\d+),(\d+)\)")]
-    private static partial Regex MyRegex();
+    private static partial Regex MulRegex();
+    
+    [GeneratedRegex(@"do\(\)")]
+    private static partial Regex DoRegex();
+    
+    [GeneratedRegex(@"don't\(\)")]
+    private static partial Regex DontRegex();
 
     public override async Task Solve(string inputPath)
     {
         var input = await GetInput(inputPath);
         var results = new List<Mul>();
 
-        foreach (var match in input.SelectMany(line => MyRegex().Matches(line)).Select(match => match. Groups))
+        foreach (var match in input.SelectMany(line => MulRegex().Matches(line)).Select(match => match. Groups))
         {
             var firstInt = int.Parse(match[1].Value);
             var secondInt = int.Parse(match[2].Value);
             results.Add(new Mul(firstInt, secondInt));
-            Console.WriteLine($"First integer: {firstInt}, Second integer: {secondInt}");
         }
 
         var sum = results.Sum(x => x.Result);
         Console.WriteLine($"Part 1: {sum}");
+        
+        var inputOneLine = string.Join("", input);
+        
+        var total = 0;
+        var allMul = MulRegex().Matches(inputOneLine).Select(match => new Mul(match.Groups, "Mul", match.Index))
+            .Concat(DoRegex().Matches(inputOneLine).Select(x => new Mul(0, 0, "Do", x.Index)))
+            .Concat(DontRegex().Matches(inputOneLine).Select(x => new Mul(0, 0, "Dont", x.Index)))
+            .OrderBy(x => x.Position)
+            .ToList();
+        
+        var enable = true;
+        foreach (var mul in allMul)
+        {
+            switch (mul.Type)
+            {
+                case "Do":
+                    enable = true;
+                    break;
+                case "Dont":
+                    enable = false;
+                    break;
+                case "Mul" when enable:
+                    total += mul.Result;
+                    break;
+            }
+        }
+
+        Console.WriteLine($"Part 2: {total}");
     }
 }
 
-public class Mul(int x, int y)
-{
+public class Mul(int x, int y, string? type = null, int position = 0)
+{ 
+    public string? Type { get; } = type;
+    public int Position { get; } = position;
     public int Result { get; } = x * y;
+
+    public Mul(GroupCollection group, string? type = null, int position = 0) 
+        : this(int.Parse(group[1].Value), int.Parse(group[2].Value), type, position)
+    {
+        Result = int.Parse(group[1].Value) * int.Parse(group[2].Value);
+    }
 }
--- 2024-advent-of-code/Program.cs
+++ 2024-advent-of-code/Program.cs
@@ -1,3 +1,5 @@
+using _2024_advent_of_code.Day01;
+using _2024_advent_of_code.Day02;
 using _2024_advent_of_code.Day03;
 
 namespace _2024_advent_of_code;
@@ -6,8 +8,34 @@ public static class Program
 {
     static async Task Main(string[] args)
     {
-        const string day = "Day03";
-        const string inputPath = $"{day}/input.txt";
+        await Day03();
+    }
+
+    private static async Task Day01()
+    {
+        const string inputPath = $"Day01/input.txt";
+        const bool test = false;
+
+        var aoc = new AoC01();
+        await (test 
+            ? aoc.Test(inputPath) 
+            : aoc.Solve(inputPath));
+    }
+    
+    private static async Task Day02()
+    {
+        const string inputPath = $"Day02/input.txt";
+        const bool test = false;
+
+        var aoc = new AoC02();
+        await (test 
+            ? aoc.Test(inputPath) 
+            : aoc.Solve(inputPath));
+    }
+    
+    private static async Task Day03()
+    {
+        const string inputPath = $"Day03/input.txt";
         const bool test = false;
 
         var aoc = new AoC03();
