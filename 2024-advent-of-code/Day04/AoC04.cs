528a9cf6718b6709741e81e3f1419acbc335ba85
David Gómez <ldavid.gomez@gmail.com>
2024-12-04 09:26:05 +0100

    AoC Day04 part2

--- 2024-advent-of-code/Day04/AoC04.cs
+++ 2024-advent-of-code/Day04/AoC04.cs
@@ -10,16 +10,19 @@ public class AoC04 : AoC
         var parsedInput = InitializeGrid(input);
         var count = CountOccurrences(parsedInput, "XMAS");
         Console.WriteLine($"Number of times XMAS appears: {count}");
+        
+        count = CountXmasOccurrences(parsedInput);
+        Console.WriteLine($"Number of times X-MAS appears: {count}");
     }
     
     private static string[,] InitializeGrid(string[] input)
     {
         var grid = new string[input.Length, input[0].Length];
-        for (var i = 0; i < input.Length; i++)
+        for (var x = 0; x < input.Length; x++)
         {
-            for (var j = 0; j < input[0].Length; j++)
+            for (var y = 0; y < input[0].Length; y++)
             {
-                grid[i, j] = input[i][j].ToString();
+                grid[x, y] = input[x][y].ToString();
             }
         }
 
@@ -33,18 +36,18 @@ public class AoC04 : AoC
         var cols = grid.GetLength(1);
         var wordLength = word.Length;
 
-        for (var i = 0; i < rows; i++)
+        for (var x = 0; x < rows; x++)
         {
-            for (var j = 0; j < cols; j++)
+            for (var y = 0; y < cols; y++)
             {
-                if (j <= cols - wordLength && IsMatch(grid, word, i, j, 0, 1)) count++;
-                if (j >= wordLength - 1 && IsMatch(grid, word, i, j, 0, -1)) count++;
-                if (i <= rows - wordLength && IsMatch(grid, word, i, j, 1, 0)) count++;
-                if (i >= wordLength - 1 && IsMatch(grid, word, i, j, -1, 0)) count++;
-                if (i <= rows - wordLength && j <= cols - wordLength && IsMatch(grid, word, i, j, 1, 1)) count++;
-                if (i >= wordLength - 1 && j >= wordLength - 1 && IsMatch(grid, word, i, j, -1, -1)) count++;
-                if (i <= rows - wordLength && j >= wordLength - 1 && IsMatch(grid, word, i, j, 1, -1)) count++;
-                if (i >= wordLength - 1 && j <= cols - wordLength && IsMatch(grid, word, i, j, -1, 1)) count++;
+                if (y <= cols - wordLength && IsMatch(grid, word, x, y, 0, 1)) count++;
+                if (y >= wordLength - 1 && IsMatch(grid, word, x, y, 0, -1)) count++;
+                if (x <= rows - wordLength && IsMatch(grid, word, x, y, 1, 0)) count++;
+                if (x >= wordLength - 1 && IsMatch(grid, word, x, y, -1, 0)) count++;
+                if (x <= rows - wordLength && y <= cols - wordLength && IsMatch(grid, word, x, y, 1, 1)) count++;
+                if (x >= wordLength - 1 && y >= wordLength - 1 && IsMatch(grid, word, x, y, -1, -1)) count++;
+                if (x <= rows - wordLength && y >= wordLength - 1 && IsMatch(grid, word, x, y, 1, -1)) count++;
+                if (x >= wordLength - 1 && y <= cols - wordLength && IsMatch(grid, word, x, y, -1, 1)) count++;
             }
         }
 
@@ -53,11 +56,50 @@ public class AoC04 : AoC
 
     private static bool IsMatch(string[,] grid, string word, int startX, int startY, int stepX, int stepY)
     {
-        for (var k = 0; k < word.Length; k++)
+        for (var i = 0; i < word.Length; i++)
         {
-            if (grid[startX + k * stepX, startY + k * stepY][0] != word[k])
+            if (grid[startX + i * stepX, startY + i * stepY][0] != word[i])
                 return false;
         }
         return true;
+    }
+    
+    private static int CountXmasOccurrences(string[,] grid)
+    {
+        var count = 0;
+        var rows = grid.GetLength(0);
+        var cols = grid.GetLength(1);
+
+        for (var x = 1; x < rows - 1; x++)
+        {
+            for (var y = 1; y < cols - 1; y++)
+            {
+                if (IsXmas(grid, x, y))
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private static bool IsXmas(string[,] grid, int x, int y)
+    {
+        return (grid[x - 1, y - 1] == "M" && grid[x - 1, y + 1] == "M" &&
+                grid[x, y] == "A" &&
+                grid[x + 1, y - 1] == "S" && grid[x + 1, y + 1] == "S")
+               || 
+               (grid[x - 1, y - 1] == "M" && grid[x - 1, y + 1] == "S" &&
+                grid[x, y] == "A" &&
+                grid[x + 1, y - 1] == "M" && grid[x + 1, y + 1] == "S")
+               || 
+               (grid[x - 1, y - 1] == "S" && grid[x - 1, y + 1] == "S" &&
+                grid[x, y] == "A" && 
+                grid[x + 1, y - 1] == "M" && grid[x + 1, y + 1] == "M")
+               || 
+               (grid[x - 1, y - 1] == "S" && grid[x - 1, y + 1] == "M" &&
+                grid[x, y] == "A" &&
+                grid[x + 1, y - 1] == "S" && grid[x + 1, y + 1] == "M");
     }
 }
